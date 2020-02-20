a4b6145f8a3a368abd6b1506c27a300bedc95fb6
Haruhiko Mima <zentsumethod@gmail.com>
2020-02-20 18:48:29 +0900

    first commit

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,46 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
+      <span>userId:{{ userId }}  message:{{ message }}</span>
     </div>
     <router-view/>
   </div>
 </template>
+
+<script>
+// Component設定
+// import InputForm from '@/components/InputForm.vue'
+
+export default {
+  name: 'app',
+  components: {
+    // InputForm
+  },
+  data() {
+    return {
+      userId: "",
+      message: "" 
+    }
+  },
+  created() {
+    const that = this
+    liff.init(
+      data => {
+        that.userId = data.context.userId
+        that.message = "初期化できたし"
+      }
+    )
+    .then(() => {
+      // start to use LIFF's api
+      // initializeApp()
+    })
+    .catch((err) => {
+        // document.getElementById("liffAppContent").classList.add('hidden');
+        // document.getElementById("liffInitErrorMessage").classList.remove('hidden');
+    })
+  }
+}
+</script>
 
 <style lang="scss">
 #app {
